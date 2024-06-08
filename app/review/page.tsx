627f4a85a31a056834f27d5dab265861fef94a77
Sisay Hiwot <angelhiwot@gmail.com>
2024-06-08 15:38:48 +0200

    add message to review page & misir img

---
+++ app/review/page.tsx
@@ -0,0 +1,14 @@
+import { Box,Text } from '@radix-ui/themes'
+import React from 'react'
+
+
+const ReviewPage = () => {
+  return (
+    <Box>
+    <Text>We&apos;re working on this page</Text>
+    </Box>
+
+  )
+}
+
+export default ReviewPage
