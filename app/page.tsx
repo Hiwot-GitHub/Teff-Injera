3de147da421a203739a187a34892d1bd8b4425cd
Sisay Hiwot <angelhiwot@gmail.com>
2024-09-29 18:24:14 +0200

    increase size of icons

--- app/page.tsx
+++ app/page.tsx
@@ -51,9 +51,9 @@ export default function Home() {
        </Flex></Box>
    
        <Box className="border-t mt-8 p-8 mx-10">
-       <Flex gapX="3">
+       <Flex gapX="6">
         <a href="tel:0791348739"><IoCall size={28} /></a>
-        <a href="https://wa.me/+250791348739"><FaWhatsapp size={28} /></a>
+        <a href="https://wa.me/+250791348739"><FaWhatsapp size={36} /></a>
       </Flex>
        </Box>
     </>
