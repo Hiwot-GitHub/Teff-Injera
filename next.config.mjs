8fdcbe77f152dd94f40d80af8ec9e626bd0ff77b
Sisay Hiwot <angelhiwot@gmail.com>
2024-10-14 19:58:16 +0200

    restore Content Security Policy

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-    async headers() {
-      return [
-        {
-          // Apply these headers to all routes in your app
-          source: '/(.*)', // Matches all routes
-          headers: [
-            {
-              key: 'Content-Security-Policy',
-              value: "default-src 'self'; script-src 'self' https://www.gstatic.com; connect-src 'self'; img-src 'self' data:; style-src 'self' 'unsafe-inline';"
-            },
-          ],
-        },
-      ];
-    },
-  };
+const nextConfig = {};
   
   export default nextConfig;
   
