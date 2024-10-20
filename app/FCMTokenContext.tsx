d0fa8f6c3a372c6887048b37dc211c426d5c36c6
Sisay Hiwot <angelhiwot@gmail.com>
2024-10-20 18:02:23 +0200

    adding use client directive to FCMTokenContext

--- app/FCMTokenContext.tsx
+++ app/FCMTokenContext.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { getToken, Messaging } from 'firebase/messaging';
 import { messaging } from '@/lib/firebase';
