c2847f3306e9f8a8f1455cb6f95dd613fc14fc03
Sisay Hiwot <angelhiwot@gmail.com>
2024-10-23 17:34:31 +0200

    presiste token with local storage

--- app/FCMTokenContext.tsx
+++ app/FCMTokenContext.tsx
@@ -8,7 +8,7 @@ interface FCMContextType {
   }
 
   const initialContextValue: FCMContextType = {
-    fcmToken: null,
+    fcmToken: null as string | null,
     setFcmToken: () => {},
   };
   
@@ -17,7 +17,24 @@ const FCMContext = createContext<FCMContextType>(initialContextValue);
 
 // Create a provider component
 export const FCMProvider = ({ children }: { children: React.ReactNode }) => {
-  const [fcmToken, setFcmToken] = useState<string | null>(null);
+  const [fcmToken, setFcmTokenState] = useState<string | null>(null);
+
+  const setFcmToken = (token: string | null) => {
+    setFcmTokenState(token);
+    if (token) {
+        localStorage.setItem('fcmToken', token); // Store in localStorage
+      } else {
+        localStorage.removeItem('fcmToken'); // Clear from localStorage if needed
+      }
+  };
+
+    // On initial load, check if an FCM token exists in localStorage
+    useEffect(() => {
+        const savedToken = localStorage.getItem('fcmToken');
+        if (savedToken) {
+          setFcmTokenState(savedToken); // Set the saved token
+        }
+      }, []);
 
   return (
     <FCMContext.Provider value={{fcmToken,  setFcmToken}}>
