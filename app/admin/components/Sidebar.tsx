4b2de6083b073f8bcc5d68833c9d491b963f786d
Sisay Hiwot <angelhiwot@gmail.com>
2024-12-30 08:15:58 +0200

    styling sidebar

--- app/admin/components/Sidebar.tsx
+++ app/admin/components/Sidebar.tsx
@@ -1,22 +1,27 @@
 import Link from 'next/link';
+import { PiNotePencilThin } from "react-icons/pi";
+import { VscDashboard } from "react-icons/vsc";
+import { MdRestaurantMenu } from "react-icons/md";
+import { MdPeopleAlt } from "react-icons/md";
 
 const Sidebar = () => {
   const menuItems = [
-    { name: 'Dashboard', href: '/admin' },
-    { name: 'Orders', href: '/admin/orders' },
-    { name: 'Menu Management', href: '/admin/menu' },
-    { name: 'Settings', href: '/admin/settings' },
+    { name: 'Dashboard', href: '/admin', icon: <VscDashboard /> },
+    { name: 'Orders', href: '/admin/orders', icon: <PiNotePencilThin /> },
+    { name: 'Menu', href: '/admin/menu', icon: <MdRestaurantMenu /> },
+    { name: 'Customers', href: '/admin/customers', icon: <MdPeopleAlt /> },
   ];
 
   return (
-    <div className="w-64 h-full bg-gray-800 text-white fixed">
+    <div className="hidden md:block w-64 h-full bg-slate-50 text-DarkGrayishBlue fixed left-0">
       <div className="p-4 text-lg font-bold">Admin Panel</div>
       <nav>
         <ul className="space-y-2">
           {menuItems.map((item) => (
             <li key={item.name}>
               <Link href={item.href}>
-                <a className="block px-4 py-2 hover:bg-gray-700">{item.name}</a>
+                <a className="block px-4 py-2 hover:bg-gray-700">
+                    <span className='mr-2'>{item.icon}</span>{item.name}</a>
               </Link>
             </li>
           ))}
